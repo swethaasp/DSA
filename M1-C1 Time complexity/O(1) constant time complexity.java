9c5cdf579ea2547aaceb8ae9979478ff74305503
Swethaa S P <97969029+swethaasp@users.noreply.github.com>
2024-08-13 16:27:49 +0530

    Create O(1) constant time complexity.java

---
+++ M1-C1 Time complexity/O(1) constant time complexity.java
@@ -0,0 +1,17 @@
+//constant time complexity 
+package module1;
+
+public class circleCircumference {
+
+	public static void main(String[] args) {
+		circleCircumference circum =new circleCircumference();
+		System.out.println(circum.circumference(4));
+
+	}
+
+	public double circumference(int radius) {
+		// TODO Auto-generated method stub
+		return 2 * Math.PI * radius;
+	}
+
+}
