69943fd0918d628b11af7d553ef1baed59001621
Swethaa S P <97969029+swethaasp@users.noreply.github.com>
2024-08-13 22:08:55 +0530

    Create bubbleSortAltO.java

---
+++ M1/C1/Sorting/bubbleSortAltO.java
@@ -0,0 +1,47 @@
+
+import java.util.* ;
+import java.io.*; 
+
+public class Solution {
+    
+    public static void bubbleSort(int[] arr, int n) {   
+        // Write your code here.
+        for(int i=n-1;i>=0;i--){
+            int didSwap=0;
+            for(int j=0;j<=i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                    didSwap=1;
+                }
+            }
+            if(didSwap==0){
+                break;
+            }
+        }
+
+    }
+
+}
+
+
+//alternate
+class Solution
+{
+    //Function to sort the array using bubble sort algorithm.
+	public static void bubbleSort(int arr[], int n)
+    {
+        //code here
+        for(int i=n-1;i>=0;i--){
+            for(int j=0;j<=i-1;j++){
+                if(arr[j]>arr[j+1]){
+                   int temp=arr[j];
+                   arr[j]=arr[j+1];
+                   arr[j+1]=temp;
+                }
+                
+            }
+        }
+    }
+}
