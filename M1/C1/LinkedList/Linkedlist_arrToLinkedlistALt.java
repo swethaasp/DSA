03e07adaf4cbe4cfbf99bcd1735c62ca55c42339
Swethaa S P <97969029+swethaasp@users.noreply.github.com>
2024-08-14 10:07:52 +0530

    Create Linkedlist_arrToLinkedlistALt.java

---
+++ M1/C1/LinkedList/Linkedlist_arrToLinkedlistALt.java
@@ -0,0 +1,45 @@
+package module1;
+
+public class Linkedlist_arrToLinkedlist {
+
+	
+   class Node{
+		int data;
+		Node next;
+		Node(int data1,Node next1){
+			this.data=data1;
+			this.next=next1;
+		}
+		
+		Node(int data1){
+			this.data=data1;
+			this.next=null;
+		}
+	}
+	
+	private  Node convertArr2Ll(int[] arr) {
+		Node head= new Node(arr[0]);
+		Node mover=head;
+		for(int i=1;i<arr.length;i++) {
+			Node temp=new Node(arr[i]);
+			mover.next=temp;
+			mover=temp;
+			
+		}
+//		/return mover;  8
+		return head; //12
+	}
+	
+
+	
+	
+	
+	public static void main(String[] args) {
+		Linkedlist_arrToLinkedlist linkedList = new Linkedlist_arrToLinkedlist();
+		int[]  arr= {12,5,6,8};
+		
+		Node head=linkedList.convertArr2Ll(arr);
+		System.out.println(head.data);
+	}
+
+}
