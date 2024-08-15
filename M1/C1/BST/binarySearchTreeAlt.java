1fa5663be4cc32ac6e4bfb61ded85450c94f7143
Swethaa S P <97969029+swethaasp@users.noreply.github.com>
2024-08-15 13:44:37 +0530

    Update binarySearchTreeAlt.java

--- M1/C1/BST/binarySearchTreeAlt.java
+++ M1/C1/BST/binarySearchTreeAlt.java
@@ -89,3 +89,64 @@ class Tree {
 
 
 //Insert a node in BST
+/*********************************************************
+
+ Following is the TreeNode structure:
+
+ class TreeNode {
+     int val;
+     TreeNode left;
+     TreeNode right;
+     TreeNode() {
+         this.val = 0;
+         this.left = null;
+         this.right = null;
+     }
+     TreeNode(int val) {
+         this.val = val;
+         this.left = null;
+         this.right = null;
+     }
+     TreeNode(int val, TreeNode left, TreeNode right) {
+         this.val = val;
+         this.left = left;
+         this.right = right;
+     }
+ };
+ ********************************************************/
+
+public class Solution {
+    public static TreeNode insertionInBST(TreeNode root, int val) {
+        // Write your code here.
+
+        if(root==null){
+            return new TreeNode(val);
+        }
+
+        TreeNode cur=root;
+        while(true){
+            if(cur.val<=val){
+                if(cur.right!=null){
+                    cur=cur.right;
+                }
+                else{
+                    cur.right= new TreeNode(val);
+                    break;
+                }
+            }
+            else{
+                if(cur.left!=null){
+                    cur=cur.left;
+                }
+                else{
+                    cur.left= new TreeNode(val);
+                    break;
+                }
+            }
+        }
+        return root;
+    }
+}
+
+
+//delete node in BST
