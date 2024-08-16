98e3b9d1dc6b3390df0f2ca21efc88a1c759933e
Swethaa S P <97969029+swethaasp@users.noreply.github.com>
2024-08-16 19:38:57 +0530

    Create StringUsuage.java

---
+++ LiveSessions/StringUsuage.java
@@ -0,0 +1,30 @@
+package module1;
+
+public class StringUsuage {
+
+	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+String a="swe";
+String b="sandy";
+ //String a ="Trainer";   //sweTrainer(reassigned value again to a)
+ String p =a+"Trainer";
+ 
+System.out.println(p);
+		
+//		StringBuffer sf =new StringBuffer("java");
+//		sf.append("Trainer");
+//		System.out.println("after append:" + sf);
+//		
+//		sf.insert(4, " IT");
+//		System.out.println("after insert:" + sf);
+//		
+//		sf.delete(3, 8);
+//		System.out.println("after delete:" + sf);
+//		
+//		sf.reverse();
+//		System.out.println("after reverse:" + sf);
+//		
+		
+	}
+
+}
