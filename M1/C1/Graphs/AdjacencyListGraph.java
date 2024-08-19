40de5c6c1917e9976ef927a85fd8c98fa1f13264
Swethaa S P <97969029+swethaasp@users.noreply.github.com>
2024-08-19 12:49:38 +0530

    Update AdjacencyListGraph.java

--- M1/C1/Graphs/AdjacencyListGraph.java
+++ M1/C1/Graphs/AdjacencyListGraph.java
@@ -8,21 +8,21 @@ public class AdjacencyListGraph {
     public AdjacencyListGraph(int nodes) {
         this.adj = new ArrayList[nodes];
         for (int i = 0; i < nodes; i++)
-            this.adj[i] = new ArrayList<>();
+            this.adj[i] = new ArrayList<>();  //0-new arraylist created 1-> new arraylist created   2-> new arraylist created .....
     }
 
     public void addEdge(int u, int v) {
-        adj[u].add(v);
+        adj[u].add(v);   //adj[u].add(v)---adj[0].add(0)---adj[0].add(1)
     }
 
     @Override
     public String toString() {
         String res = "";
-        for (int i = 0; i < adj.length; i++) {
-            res += (i + ":");
-            for (int j = 0; j < adj[i].size(); j++)
-                res += (" " + adj[i].get(j));
-            if (i + 1 < adj.length)
+        for (int i = 0; i < adj.length; i++) {  //0  to 5
+            res += (i + ":");   //0:   1:
+            for (int j = 0; j < adj[i].size(); j++)   //0.size()-2
+                res += (" " + adj[i].get(j));   //1 3 
+            if (i + 1 < adj.length)  //nextline
                 res += "\n";
         }
         return res;
@@ -41,3 +41,22 @@ public class AdjacencyListGraph {
         System.out.println(g);
     }
 }
+
+The graph can be visualized as follows:
+0 -> 1, 3
+1 -> 4
+2 -> 4, 5
+3 -> 1
+4 -> 3
+5 -> 5
+
+
+//output
+/*
+0: 1 3
+1: 4
+2: 4 5
+3: 1
+4: 3
+5: 5
+*/
