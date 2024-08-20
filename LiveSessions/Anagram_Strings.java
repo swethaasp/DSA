a34de130ebce97620c4e79ea3e72e49c669b1374
Swethaa S P <97969029+swethaasp@users.noreply.github.com>
2024-08-20 21:42:24 +0530

    Update Anagram_Strings.java

--- LiveSessions/Anagram_Strings.java
+++ LiveSessions/Anagram_Strings.java
@@ -52,3 +52,38 @@ public class Anagram_Strings {
 	}
  
 }
+
+
+
+
+
+// another
+
+public class AnagramFinder {
+    public static void findAnagrams(String[] words) {
+        for (int i = 0; i < words.length; i++) {
+            for (int j = i + 1; j < words.length; j++) {
+                if (areAnagrams(words[i], words[j])) {
+                    System.out.println(words[i] + " and " + words[j] + " are anagrams");
+                }
+            }
+        }
+    }
+
+    private static boolean areAnagrams(String word1, String word2) {
+        if (word1.length() != word2.length()) {
+            return false;
+        }
+
+        char[] charArray1 = word1.toCharArray();
+        char[] charArray2 = word2.toCharArray();
+        Arrays.sort(charArray1);
+        Arrays.sort(charArray2);
+        return Arrays.equals(charArray1, charArray2);
+    }
+
+    public static void main(String[] args) {
+        String[] wordArr = {"eat", "tea", "tan", "ate", "nat", "bat"};
+        findAnagrams(wordArr);
+    }
+}
